24315d578b1ef16e1a57d36b34fd9f99b1a366e6
TuFarm <kpnkt.vn@gmail.com>
2024-03-02 17:35:00 +0700

    abc

---
+++ music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+    {
+      name: "See You Again",
+      artist: "Charlie Puth",
+      img: "CP-SeeYouAgain",
+      src: "CP-SeeYouAgain"
+    },
+    {
+      name: "Attention",
+      artist: "Charlie Puth",
+      img: "CP-Attention",
+      src: "CP-Attention"
+    },
+    {
+      name: "We don't talk anymore",
+      artist: "Charlie Puth",
+      img: "CP-WeDontTalkAnymore",
+      src: "CP-WeDontTalkAnymore"
+    },
+    {
+      name: "The Night",
+      artist: "Avicii",
+      img: "Avicii-TheNights",
+      src: "Avicii-TheNights"
+    },
+    {
+      name: "Waiting For Love",
+      artist: "Avicii",
+      img: "Avicii-WaitingForLove",
+      src: "Avicii-WaitingForLove"
+    },
+
+]
